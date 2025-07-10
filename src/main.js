80b40e75a6a711af0bded6514e0cb7a065312b99
tkornilova <kornilova.tat07@gmail.com>
2025-07-11 02:10:41 +0400

    feat: добавляет расчет премий

--- src/main.js
+++ src/main.js
@@ -88,7 +88,14 @@ function analyzeSalesData(data, options) {
     // Сортировка продавцов по прибыли
     sellerStats.sort((a, b) => b.profit - a.profit);
 
-    // @TODO: Назначение премий на основе ранжирования
+    // Назначение премий на основе ранжирования
+    sellerStats.forEach((seller, index) => {
+        seller.bonus = calculateBonusByProfit(index, sellerStats.length, seller);
+        seller.top_products = Object.entries(seller.products_sold)
+            .map(([sku, quantity]) => ({ sku, quantity }))
+            .sort((a, b) => b.quantity - a.quantity)
+            .slice(0, 10);
+    })
 
     // @TODO: Подготовка итоговой коллекции с нужными полями
 }
